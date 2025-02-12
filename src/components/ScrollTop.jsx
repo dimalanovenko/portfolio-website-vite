d26c864212defab22d1e693c95b28929a91d731b
Dima <dimalanovenko0@gmail.com>
2025-02-12 16:21:09 +0100

    deploy

--- src/components/ScrollTop.jsx
+++ src/components/ScrollTop.jsx
@@ -28,13 +28,14 @@ const ScrollToTop = () => {
         <button
             onClick={scrollToTop}
             className={`cursor-pointer flex items-center justify-center fixed lg:bottom-15 lg:right-15 bottom-7 right-7
-                w-22 h-22 bg-zinc-800 text-pink-400 rounded-full shadow-lg text-2xl 
-                transition-all duration-300 hover:bg-pink-400 hover:text-white ${
-                isVisible ? "opacity-100" : "opacity-0 pointer-events-none"
-            }`}
+                w-22 h-22 rounded-full shadow-lg text-2xl 
+                transition-all duration-300 
+                ${theme === "dark" ? 'bg-zinc-800 text-pink-400 hover:bg-pink-400 hover:text-white'
+                : 'bg-blue-500 text-white hover:bg-white hover:text-blue-500'}
+                ${isVisible ? "opacity-100" : "opacity-0 pointer-events-none"}`}
         >
             <BiSolidUpArrow
-                className={`w-10 h-10 ${theme === 'dark' ? '' : 'text-white'}`}
+                className='w-10 h-10'
             />
         </button>
     );
