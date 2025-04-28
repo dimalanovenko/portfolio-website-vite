d0b5a63c6edebe61cca002c2f9f3ad00031d441c
Dima <dimalanovenko0@gmail.com>
2025-04-28 22:57:29 +0200

    updated tech and projects section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,19 +1,16 @@
-import {useTheme} from "./theme/ThemeProvider.jsx";
+import { useTheme } from "./theme/ThemeProvider.jsx";
 import Title from "./UI/Title.jsx";
-import {IoArrowRedoSharp} from "react-icons/io5";
+import { IoArrowRedoSharp } from "react-icons/io5";
 import portfolioImgDark from "../assets/img/portfolioWebsiteDark.png"
 import portfolioImgLight from "../assets/img/portfolioWebsiteLight.png"
-import {motion} from "framer-motion";
-import {useEffect, useState} from "react";
+import socialNetwork from "../assets/img/socialNetwork.png"
+import projectsManager from '../assets/img/projectsManager.png'
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 const Projects = () => {
-    const {theme} = useTheme()
+    const { theme } = useTheme()
     const [isVisible, setIsVisible] = useState(false);
-
-    const cards = [
-        {id: 1},
-        {id: 2},
-    ]
 
     useEffect(() => {
         const handleScroll = () => {
@@ -29,14 +26,14 @@ const Projects = () => {
     }, []);
 
     const scrollToTop = () => {
-        window.scrollTo({top: 0, behavior: "smooth"});
+        window.scrollTo({ top: 0, behavior: "smooth" });
     };
 
     return (
         <section className={`w-full py-20 flex flex-col items-center 
             ${theme === 'dark' ? 'bg-dark' : 'bg-light'}`}>
             <div className="w-10/12 mx-auto flex flex-col items-center">
-                <Title title="Projects"/>
+                <Title title="Projects" />
                 <div className="grid lg:grid-cols-3 grid-cols-1 gap-12">
                     <div className={`${theme === 'light' ? 'bg-gray-200 text-zinc-800' : 'bg-zinc-800 text-white'} 
                         w-80 h-80 rounded-xl flex flex-col items-center justify-around `}>
@@ -45,13 +42,13 @@ const Projects = () => {
                             className={`flex items-center gap-1 cursor-pointer font-bold text-xl 
                             ${theme === 'dark' ? 'hover:text-pink-400' : 'hover:text-blue-500'}`}>
                             <a>Portfolio Website</a>
-                            <IoArrowRedoSharp/>
+                            <IoArrowRedoSharp />
                         </div>
                         <motion.div
                             className="cursor-pointer"
                             onClick={scrollToTop}
-                            whileHover={{scale: 1.1}}
-                            transition={{duration: 0.4}}
+                            whileHover={{ scale: 1.1 }}
+                            transition={{ duration: 0.4 }}
                         >
                             {theme === 'dark' ?
                                 <img
@@ -83,44 +80,82 @@ const Projects = () => {
                             </div>
                         </div>
                     </div>
-                    {cards.map(card => (
-                        <div
-                            key={card.id}
-                            className={`${theme === 'light' ? 'bg-gray-200 text-zinc-800' : 'bg-zinc-800 text-white'} 
+                    <div className={`${theme === 'light' ? 'bg-gray-200 text-zinc-800' : 'bg-zinc-800 text-white'} 
                         w-80 h-80 rounded-xl flex flex-col items-center justify-around `}>
-                            <div className={`flex items-center gap-1 cursor-pointer font-bold text-xl 
+                        <a
+                            href="https://github.com/dimalanovenko/sozialesNetzwerk"
+                            className={`flex items-center gap-1 cursor-pointer font-bold text-xl 
                             ${theme === 'dark' ? 'hover:text-pink-400' : 'hover:text-blue-500'}`}>
-                                <a>Coming soon...</a>
-                                <IoArrowRedoSharp/>
-                            </div>
-                            <motion.div
-                                className="cursor-pointer"
-                                whileHover={{scale: 1.1}}
-                                transition={{duration: 0.4}}
-                            >
-                                {theme === 'dark' ?
-                                    <div className="w-[252px] h-[144px] rounded bg-zinc-900"/>
-                                    :
-                                    <div className="w-[252px] h-[144px] rounded bg-white"/>
-                                }
+                            <a>Social Media Website</a>
+                            <IoArrowRedoSharp />
+                        </a>
+                        <motion.div
+                            className="cursor-pointer"
+                            onClick={scrollToTop}
+                            whileHover={{ scale: 1.1 }}
+                            transition={{ duration: 0.4 }}
+                        >
 
-                            </motion.div>
-                            <div className="w-full flex justify-center items-center gap-5">
-                                <div
-                                    className={`py-1.5 px-3 rounded-3xl text-white ${theme === 'dark' ? 'bg-pink-400' : 'bg-blue-500'}`}>
-                                    <h1>tech</h1>
-                                </div>
-                                <div
-                                    className={`py-1.5 px-3 rounded-3xl text-white ${theme === 'dark' ? 'bg-pink-400' : 'bg-blue-500'}`}>
-                                    <h1>tech</h1>
-                                </div>
-                                <div
-                                    className={`py-1.5 px-3 rounded-3xl text-white ${theme === 'dark' ? 'bg-pink-400' : 'bg-blue-500'}`}>
-                                    <h1>tech</h1>
-                                </div>
+                            <a href="https://github.com/dimalanovenko/sozialesNetzwerk">
+                                <img
+                                    className="w-[252px] h-[144px] rounded"
+                                    src={socialNetwork}
+                                    alt="socialNetwork"
+                                />
+                            </a>
+                        </motion.div>
+                        <div className="w-full flex justify-center items-center gap-5">
+                            <div
+                                className={`py-1.5 px-3 rounded-3xl text-white ${theme === 'dark' ? 'bg-pink-400' : 'bg-blue-500'}`}>
+                                <h1>rtk</h1>
+                            </div>
+                            <div
+                                className={`py-1.5 px-3 rounded-3xl text-white ${theme === 'dark' ? 'bg-pink-400' : 'bg-blue-500'}`}>
+                                <h1>react</h1>
+                            </div>
+                            <div
+                                className={`py-1.5 px-3 rounded-3xl text-white ${theme === 'dark' ? 'bg-pink-400' : 'bg-blue-500'}`}>
+                                <h1>tailwind css</h1>
                             </div>
                         </div>
-                    ))}
+                    </div>
+                    <div className={`${theme === 'light' ? 'bg-gray-200 text-zinc-800' : 'bg-zinc-800 text-white'} 
+                        w-80 h-80 rounded-xl flex flex-col items-center justify-around `}>
+                        <a
+                            href="https://github.com/dimalanovenko/trood-frontend-hackathon"
+                            className={`flex items-center gap-1 cursor-pointer font-bold text-xl 
+                            ${theme === 'dark' ? 'hover:text-pink-400' : 'hover:text-blue-500'}`}>
+                            <a>Projects Manager</a>
+                            <IoArrowRedoSharp />
+                        </a>
+                        <motion.div
+                            className="cursor-pointer"
+                            whileHover={{ scale: 1.1 }}
+                            transition={{ duration: 0.4 }}
+                        >
+                            <a href="https://github.com/dimalanovenko/trood-frontend-hackathon">
+                                <img
+                                    className="w-[252px] h-[144px] rounded"
+                                    src={projectsManager}
+                                    alt="projectsManager"
+                                />
+                            </a>
+                        </motion.div>
+                        <div className="w-full flex justify-center items-center gap-5">
+                            <div
+                                className={`py-1.5 px-3 rounded-3xl text-white ${theme === 'dark' ? 'bg-pink-400' : 'bg-blue-500'}`}>
+                                <h1>ts</h1>
+                            </div>
+                            <div
+                                className={`py-1.5 px-3 rounded-3xl text-white ${theme === 'dark' ? 'bg-pink-400' : 'bg-blue-500'}`}>
+                                <h1>rtk query</h1>
+                            </div>
+                            <div
+                                className={`py-1.5 px-3 rounded-3xl text-white ${theme === 'dark' ? 'bg-pink-400' : 'bg-blue-500'}`}>
+                                <h1>react</h1>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,18 +1,22 @@
-import {motion} from "framer-motion"
-import {FaReact} from "react-icons/fa";
-import {FaJsSquare} from "react-icons/fa";
-import {FaHtml5} from "react-icons/fa";
-import {FaCss3Alt} from "react-icons/fa";
-import {RiTailwindCssFill} from "react-icons/ri";
-import {SiRedux, SiSass} from "react-icons/si";
-import {FaGitAlt} from "react-icons/fa";
-import {SiGnubash} from "react-icons/si";
-import {useTheme} from './theme/ThemeProvider.jsx'
+import { motion } from "framer-motion"
+import { FaReact } from "react-icons/fa";
+import { FaJsSquare } from "react-icons/fa";
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { SiRedux, SiSass } from "react-icons/si";
+import reactRouterImg from '../assets/img/react-router.png'
+import { FaGitAlt } from "react-icons/fa";
+import { SiGnubash } from "react-icons/si";
+import { BiLogoTypescript } from "react-icons/bi";
+import { IoLogoNodejs } from "react-icons/io";
+import { useTheme } from './theme/ThemeProvider.jsx'
 import Title from "./UI/Title.jsx";
-import {useState} from "react";
+import { useState } from "react";
+import { img } from "framer-motion/client";
 
 const Tech = () => {
-    const {theme} = useTheme();
+    const { theme } = useTheme();
     const [flipped, setFlipped] = useState({
         rotateReact: false,
         rotateJs: false,
@@ -23,13 +27,16 @@ const Tech = () => {
         rotateGit: false,
         rotateBash: false,
         rotateRedux: false,
+        rotateTS: false,
+        rotateNode: false,
+        rotateRouter: false
     });
 
     return (
         <section
             className={`${theme === 'dark' ? 'bg-dark' : 'bg-light'}`}
         >
-            <Title title="Technologies"/>
+            <Title title="Technologies" />
             <div
                 className='w-10/12 mx-auto flex flex-col justify-between items-center'
             >
@@ -37,10 +44,64 @@ const Tech = () => {
                     className='grid lg:grid-cols-3 grid-cols-1 gap-21 lg:mt-0 mt-10'
                 >
                     <motion.li
-                        whileHover={{scale: 1.2}}
-                        animate={{rotateY: flipped.rotateRedux ? 180 : 0}}
-                        transition={{duration: 0.4}}
-                        onClick={() => setFlipped({...flipped, rotateRedux: !flipped.rotateRedux})}
+                        whileHover={{ scale: 1.2 }}
+                        animate={{ rotateY: flipped.rotateNode ? 180 : 0 }}
+                        transition={{ duration: 0.4 }}
+                        onClick={() => setFlipped({ ...flipped, rotateNode: !flipped.rotateNode })}
+                        className={`${theme === 'dark' ? 'bg-zinc-800' : 'bg-gray-200'}  
+                        relative cursor-pointer rounded-xl lg:w-70 lg:h-35 w-80 h-40 flex items-center justify-center`}
+                    >
+                        {!flipped.rotateNode ?
+                            <IoLogoNodejs
+                                className='lg:h-15 lg:w-15 h-20 w-20 text-[#7fba00]'
+                            />
+                            :
+                            <motion.h1
+                                initial={{
+                                    rotateY: flipped.rotateNode ? 180 : 0,
+                                    opacity: 0
+                                }}
+                                animate={{
+                                    rotateY: flipped.rotateNode ? 180 : 0,
+                                    opacity: flipped.rotateNode ? 1 : 0,
+                                }}
+                                className={`lg:text-2xl text-3xl ${theme === 'dark' ? 'text-white' : 'text-zinc-800'}`}>
+                                Node.js
+                            </motion.h1>
+                        }
+                    </motion.li>
+                    <motion.li
+                        whileHover={{ scale: 1.2 }}
+                        animate={{ rotateY: flipped.rotateTS ? 180 : 0 }}
+                        transition={{ duration: 0.4 }}
+                        onClick={() => setFlipped({ ...flipped, rotateTS: !flipped.rotateTS })}
+                        className={`${theme === 'dark' ? 'bg-zinc-800' : 'bg-gray-200'}  
+                        relative cursor-pointer rounded-xl lg:w-70 lg:h-35 w-80 h-40 flex items-center justify-center`}
+                    >
+                        {!flipped.rotateTS ?
+                            <BiLogoTypescript
+                                className='lg:h-15 lg:w-15 h-20 w-20 text-[#3178c6]'
+                            />
+                            :
+                            <motion.h1
+                                initial={{
+                                    rotateY: flipped.rotateTS ? 180 : 0,
+                                    opacity: 0
+                                }}
+                                animate={{
+                                    rotateY: flipped.rotateTS ? 180 : 0,
+                                    opacity: flipped.rotateTS ? 1 : 0,
+                                }}
+                                className={`lg:text-2xl text-3xl ${theme === 'dark' ? 'text-white' : 'text-zinc-800'}`}>
+                                Typescript
+                            </motion.h1>
+                        }
+                    </motion.li>
+                    <motion.li
+                        whileHover={{ scale: 1.2 }}
+                        animate={{ rotateY: flipped.rotateRedux ? 180 : 0 }}
+                        transition={{ duration: 0.4 }}
+                        onClick={() => setFlipped({ ...flipped, rotateRedux: !flipped.rotateRedux })}
                         className={`${theme === 'dark' ? 'bg-zinc-800' : 'bg-gray-200'}  
                         relative cursor-pointer rounded-xl lg:w-70 lg:h-35 w-80 h-40 flex items-center justify-center`}
                     >
@@ -59,15 +120,40 @@ const Tech = () => {
                                     opacity: flipped.rotateRedux ? 1 : 0,
                                 }}
                                 className={`lg:text-2xl text-3xl ${theme === 'dark' ? 'text-white' : 'text-zinc-800'}`}>
-                                redux + toolkit
+                                Redux + RTK
                             </motion.h1>
                         }
                     </motion.li>
                     <motion.li
-                        whileHover={{scale: 1.2}}
-                        animate={{rotateY: flipped.rotateReact ? 180 : 0}}
-                        transition={{duration: 0.4}}
-                        onClick={() => setFlipped({...flipped, rotateReact: !flipped.rotateReact})}
+                        whileHover={{ scale: 1.2 }}
+                        animate={{ rotateY: flipped.rotateRouter ? 180 : 0 }}
+                        transition={{ duration: 0.4 }}
+                        onClick={() => setFlipped({ ...flipped, rotateRouter: !flipped.rotateRouter })}
+                        className={`${theme === 'dark' ? 'bg-zinc-800' : 'bg-gray-200'}  
+                        relative cursor-pointer rounded-xl lg:w-70 lg:h-35 w-80 h-40 flex items-center justify-center`}
+                    >
+                        {!flipped.rotateRouter ?
+                            <img className="w-25 h-15 lg:w-20 lg:h-12" src={reactRouterImg} alt="react-router" />
+                            :
+                            <motion.h1
+                                initial={{
+                                    rotateY: flipped.rotateRouter ? 180 : 0,
+                                    opacity: 0
+                                }}
+                                animate={{
+                                    rotateY: flipped.rotateRouter ? 180 : 0,
+                                    opacity: flipped.rotateRouter ? 1 : 0,
+                                }}
+                                className={`lg:text-2xl text-3xl ${theme === 'dark' ? 'text-white' : 'text-zinc-800'}`}>
+                                React-router
+                            </motion.h1>
+                        }
+                    </motion.li>
+                    <motion.li
+                        whileHover={{ scale: 1.2 }}
+                        animate={{ rotateY: flipped.rotateReact ? 180 : 0 }}
+                        transition={{ duration: 0.4 }}
+                        onClick={() => setFlipped({ ...flipped, rotateReact: !flipped.rotateReact })}
                         className={`${theme === 'dark' ? 'bg-zinc-800' : 'bg-gray-200'}  
                         relative cursor-pointer rounded-xl lg:w-70 lg:h-35 w-80 h-40 flex items-center justify-center`}
                     >
@@ -91,10 +177,10 @@ const Tech = () => {
                         }
                     </motion.li>
                     <motion.li
-                        whileHover={{scale: 1.2}}
-                        animate={{rotateY: flipped.rotateTailwind ? 180 : 0}}
-                        transition={{duration: 0.4}}
-                        onClick={() => setFlipped({...flipped, rotateTailwind: !flipped.rotateTailwind})}
+                        whileHover={{ scale: 1.2 }}
+                        animate={{ rotateY: flipped.rotateTailwind ? 180 : 0 }}
+                        transition={{ duration: 0.4 }}
+                        onClick={() => setFlipped({ ...flipped, rotateTailwind: !flipped.rotateTailwind })}
                         className={`${theme === 'dark' ? 'bg-zinc-800' : 'bg-gray-200'}  
                         relative cursor-pointer rounded-xl lg:w-70 lg:h-35 w-80 h-40 flex items-center justify-center`}
                     >
@@ -113,15 +199,15 @@ const Tech = () => {
                                     opacity: flipped.rotateTailwind ? 1 : 0,
                                 }}
                                 className={`lg:text-2xl text-3xl ${theme === 'dark' ? 'text-white' : 'text-zinc-800'}`}>
-                                tailwind
+                                Tailwind CSS
                             </motion.h1>
                         }
                     </motion.li>
                     <motion.li
-                        whileHover={{scale: 1.2}}
-                        animate={{rotateY: flipped.rotateSass ? 180 : 0}}
-                        transition={{duration: 0.4}}
-                        onClick={() => setFlipped({...flipped, rotateSass: !flipped.rotateSass})}
+                        whileHover={{ scale: 1.2 }}
+                        animate={{ rotateY: flipped.rotateSass ? 180 : 0 }}
+                        transition={{ duration: 0.4 }}
+                        onClick={() => setFlipped({ ...flipped, rotateSass: !flipped.rotateSass })}
                         className={`${theme === 'dark' ? 'bg-zinc-800' : 'bg-gray-200'}  
                         relative cursor-pointer rounded-xl lg:w-70 lg:h-35 w-80 h-40 flex items-center justify-center`}
                     >
@@ -140,15 +226,15 @@ const Tech = () => {
                                     opacity: flipped.rotateSass ? 1 : 0,
                                 }}
                                 className={`lg:text-2xl text-3xl ${theme === 'dark' ? 'text-white' : 'text-zinc-800'}`}>
-                                sass
+                                SCSS
                             </motion.h1>
                         }
                     </motion.li>
                     <motion.li
-                        whileHover={{scale: 1.2}}
-                        animate={{rotateY: flipped.rotateBash ? 180 : 0}}
-                        transition={{duration: 0.4}}
-                        onClick={() => setFlipped({...flipped, rotateBash: !flipped.rotateBash})}
+                        whileHover={{ scale: 1.2 }}
+                        animate={{ rotateY: flipped.rotateBash ? 180 : 0 }}
+                        transition={{ duration: 0.4 }}
+                        onClick={() => setFlipped({ ...flipped, rotateBash: !flipped.rotateBash })}
                         className={`${theme === 'dark' ? 'bg-zinc-800' : 'bg-gray-200'}  
                         relative cursor-pointer rounded-xl lg:w-70 lg:h-35 w-80 h-40 flex items-center justify-center`}
                     >
@@ -167,15 +253,15 @@ const Tech = () => {
                                     opacity: flipped.rotateBash ? 1 : 0,
                                 }}
                                 className={`lg:text-2xl text-3xl ${theme === 'dark' ? 'text-white' : 'text-zinc-800'}`}>
-                                bash
+                                Bash
                             </motion.h1>
                         }
                     </motion.li>
                     <motion.li
-                        whileHover={{scale: 1.2}}
-                        animate={{rotateY: flipped.rotateGit ? 180 : 0}}
-                        transition={{duration: 0.4}}
-                        onClick={() => setFlipped({...flipped, rotateGit: !flipped.rotateGit})}
+                        whileHover={{ scale: 1.2 }}
+                        animate={{ rotateY: flipped.rotateGit ? 180 : 0 }}
+                        transition={{ duration: 0.4 }}
+                        onClick={() => setFlipped({ ...flipped, rotateGit: !flipped.rotateGit })}
                         className={`${theme === 'dark' ? 'bg-zinc-800' : 'bg-gray-200'}  
                         relative cursor-pointer rounded-xl lg:w-70 lg:h-35 w-80 h-40 flex items-center justify-center`}
                     >
@@ -194,15 +280,15 @@ const Tech = () => {
                                     opacity: flipped.rotateGit ? 1 : 0,
                                 }}
                                 className={`lg:text-2xl text-3xl ${theme === 'dark' ? 'text-white' : 'text-zinc-800'}`}>
-                                git
+                                Git
                             </motion.h1>
                         }
                     </motion.li>
                     <motion.li
-                        whileHover={{scale: 1.2}}
-                        animate={{rotateY: flipped.rotateJs ? 180 : 0}}
-                        transition={{duration: 0.3}}
-                        onClick={() => setFlipped({...flipped, rotateJs: !flipped.rotateJs})}
+                        whileHover={{ scale: 1.2 }}
+                        animate={{ rotateY: flipped.rotateJs ? 180 : 0 }}
+                        transition={{ duration: 0.3 }}
+                        onClick={() => setFlipped({ ...flipped, rotateJs: !flipped.rotateJs })}
                         className={`${theme === 'dark' ? 'bg-zinc-800' : 'bg-gray-200'}  
                         relative cursor-pointer rounded-xl lg:w-70 lg:h-35 w-80 h-40 flex items-center justify-center`}
                     >
@@ -221,15 +307,15 @@ const Tech = () => {
                                     opacity: flipped.rotateJs ? 1 : 0,
                                 }}
                                 className={`lg:text-2xl text-3xl ${theme === 'dark' ? 'text-white' : 'text-zinc-800'}`}>
-                                js
+                                JavaScript
                             </motion.h1>
                         }
                     </motion.li>
                     <motion.li
-                        whileHover={{scale: 1.2}}
-                        animate={{rotateY: flipped.rotateCss ? 180 : 0}}
-                        transition={{duration: 0.4}}
-                        onClick={() => setFlipped({...flipped, rotateCss: !flipped.rotateCss})}
+                        whileHover={{ scale: 1.2 }}
+                        animate={{ rotateY: flipped.rotateCss ? 180 : 0 }}
+                        transition={{ duration: 0.4 }}
+                        onClick={() => setFlipped({ ...flipped, rotateCss: !flipped.rotateCss })}
                         className={`${theme === 'dark' ? 'bg-zinc-800' : 'bg-gray-200'}  
                         relative cursor-pointer rounded-xl lg:w-70 lg:h-35 w-80 h-40 flex items-center justify-center`}
                     >
@@ -248,15 +334,15 @@ const Tech = () => {
                                     opacity: flipped.rotateCss ? 1 : 0,
                                 }}
                                 className={`lg:text-2xl text-3xl ${theme === 'dark' ? 'text-white' : 'text-zinc-800'}`}>
-                                css
+                                CSS3
                             </motion.h1>
                         }
                     </motion.li>
                     <motion.li
-                        whileHover={{scale: 1.2}}
-                        animate={{rotateY: flipped.rotateHtml ? 180 : 0}}
-                        transition={{duration: 0.4}}
-                        onClick={() => setFlipped({...flipped, rotateHtml: !flipped.rotateHtml})}
+                        whileHover={{ scale: 1.2 }}
+                        animate={{ rotateY: flipped.rotateHtml ? 180 : 0 }}
+                        transition={{ duration: 0.4 }}
+                        onClick={() => setFlipped({ ...flipped, rotateHtml: !flipped.rotateHtml })}
                         className={`${theme === 'dark' ? 'bg-zinc-800' : 'bg-gray-200'}  
                         relative cursor-pointer rounded-xl lg:w-70 lg:h-35 w-80 h-40 flex items-center justify-center`}
                     >
@@ -275,7 +361,7 @@ const Tech = () => {
                                     opacity: flipped.rotateHtml ? 1 : 0,
                                 }}
                                 className={`lg:text-2xl text-3xl ${theme === 'dark' ? 'text-white' : 'text-zinc-800'}`}>
-                                html
+                                HTML5
                             </motion.h1>
                         }
                     </motion.li>
